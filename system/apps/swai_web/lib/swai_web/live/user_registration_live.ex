bc21b83e52f8443680d3a6cd01820f4727f3cc2f
Rafael Lefever <rafael.lefever@discomco.pl>
2024-07-01 13:38:10 +0200

    updated seeds method

--- system/apps/swai_web/lib/swai_web/live/user_registration_live.ex
+++ system/apps/swai_web/lib/swai_web/live/user_registration_live.ex
@@ -3,7 +3,82 @@ defmodule SwaiWeb.UserRegistrationLive do
 
   alias Swai.Accounts
   alias Swai.Accounts.User
+  alias Edges.Service, as: Edges
 
+  @impl true
+  def mount(_params, _session, socket) do
+    changeset = Accounts.change_user_registration(%User{})
+
+    case connected?(socket) do
+      true ->
+        {:ok,
+         socket
+         |> assign(
+           trigger_submit: false,
+           check_errors: false,
+           edges: Edges.get_all()
+         )
+         |> assign_form(changeset), temporary_assigns: [form: nil]}
+
+      false ->
+        {:ok,
+         socket
+         |> assign(
+           trigger_submit: false,
+           check_errors: false,
+           edges: []
+         )
+         |> assign_form(changeset), temporary_assigns: [form: nil]}
+    end
+  end
+
+  
+  @impl true
+  def handle_info(_msg, socket) do
+    {
+      :noreply,
+      socket
+      |> assign(
+        edges: Edges.get_all()
+      )
+    }
+  end
+
+  @impl true
+  def handle_event("save", %{"user" => user_params}, socket) do
+    case Accounts.register_user(user_params) do
+      {:ok, user} ->
+        {:ok, _} =
+          Accounts.deliver_user_confirmation_instructions(
+            user,
+            &url(~p"/users/confirm/#{&1}")
+          )
+
+        changeset = Accounts.change_user_registration(user)
+        {:noreply, socket |> assign(trigger_submit: true) |> assign_form(changeset)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, socket |> assign(check_errors: true) |> assign_form(changeset)}
+    end
+  end
+
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset = Accounts.change_user_registration(%User{}, user_params)
+    {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    form = to_form(changeset, as: "user")
+
+    if changeset.valid? do
+      assign(socket, form: form, check_errors: false)
+    else
+      assign(socket, form: form)
+    end
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
     <div class="max-w-sm mx-auto">
@@ -40,48 +115,5 @@ defmodule SwaiWeb.UserRegistrationLive do
       </.simple_form>
     </div>
     """
-  end
-
-  def mount(_params, _session, socket) do
-    changeset = Accounts.change_user_registration(%User{})
-
-    socket =
-      socket
-      |> assign(trigger_submit: false, check_errors: false)
-      |> assign_form(changeset)
-
-    {:ok, socket, temporary_assigns: [form: nil]}
-  end
-
-  def handle_event("save", %{"user" => user_params}, socket) do
-    case Accounts.register_user(user_params) do
-      {:ok, user} ->
-        {:ok, _} =
-          Accounts.deliver_user_confirmation_instructions(
-            user,
-            &url(~p"/users/confirm/#{&1}")
-          )
-
-        changeset = Accounts.change_user_registration(user)
-        {:noreply, socket |> assign(trigger_submit: true) |> assign_form(changeset)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, socket |> assign(check_errors: true) |> assign_form(changeset)}
-    end
-  end
-
-  def handle_event("validate", %{"user" => user_params}, socket) do
-    changeset = Accounts.change_user_registration(%User{}, user_params)
-    {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
-  end
-
-  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
-    form = to_form(changeset, as: "user")
-
-    if changeset.valid? do
-      assign(socket, form: form, check_errors: false)
-    else
-      assign(socket, form: form)
-    end
   end
 end
