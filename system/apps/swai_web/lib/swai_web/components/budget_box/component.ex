15ce01aaabdc69ddb8d909c76187b705a673f82c
Rafael Lefever <rafael.lefever@discomco.pl>
2024-08-15 19:44:45 +0200

    populate shows mission

--- system/apps/swai_web/lib/swai_web/components/budget_box/component.ex
+++ system/apps/swai_web/lib/swai_web/components/budget_box/component.ex
@@ -1,7 +1,7 @@
 defmodule SwaiWeb.BudgetBox do
   use SwaiWeb, :live_component
 
-  
+
   @impl true
   def update(assigns, socket) do
     {:ok, socket |> assign(assigns)}
@@ -14,8 +14,15 @@ defmodule SwaiWeb.BudgetBox do
       <span class=" text-xs font-regular text-gray-700 pl-2" id={"budget_info_#{@current_user.id}"}>
         <%= @current_user.budget %> <span> 👾 </span>
         <%!-- <%= if @user.budget <= 0 do %> --%>
-        <.link
+        <%!-- <.link
           href={"/populate/#{@current_user.id}"}
+          class="bg-swBrand-dark hover:bg-swBrand text-white font-regular py-1 px-3 text-xs rounded-lg transition duration-300 ease-in-out"
+          role="button"
+          method="get"
+          id="button_order_drones"
+        > --%>
+        <.link
+          href={"/mission"}
           class="bg-swBrand-dark hover:bg-swBrand text-white font-regular py-1 px-3 text-xs rounded-lg transition duration-300 ease-in-out"
           role="button"
           method="get"
